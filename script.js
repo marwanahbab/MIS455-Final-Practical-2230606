7b3e33351c082d20660f6f5dd369434c928b5092
marwanahbab <marwanaeltertius@gmail.com>
2025-05-08 16:08:29 +0600

    added some default vars

---
+++ script.js
@@ -0,0 +1,3 @@
+var searchForm = document.getElementById('search-form');
+var countryInput = document.getElementById('country-input');
+var resultsContainer = document.getElementById('results-container');
