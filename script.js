59f4e2aaa88f675d403a1d350e725d67125510e9
marwanahbab <marwanaeltertius@gmail.com>
2025-05-08 16:10:17 +0600

    added script to for search bar function error

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var searchForm = document.getElementById('search-form');
 var countryInput = document.getElementById('country-input');
 var resultsContainer = document.getElementById('results-container');
+
+searchForm.onsubmit = function(event) {
+    event.preventDefault();
+    resultsContainer.innerHTML = '';
+    getCountryInfo(countryInput.value);
+    return false;
+};
